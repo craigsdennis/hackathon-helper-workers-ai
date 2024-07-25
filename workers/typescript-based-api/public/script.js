640d331eb8dad6cd4be06d189447c4f6aa356fbb
Craig Dennis <craigsdennis@gmail.com>
2024-07-24 21:22:53 -0700

    Adds a new TypeScript example

---
+++ workers/typescript-based-api/public/script.js
@@ -0,0 +1,29 @@
+document.getElementById('searchButton').addEventListener('click', async function () {
+	const word = document.getElementById('wordInput').value;
+	const resultSection = document.getElementById('resultSection');
+	const definition = document.getElementById('definition');
+
+	if (word.trim() === '') {
+		alert('Please enter a word');
+		return;
+	}
+
+	const response = await fetch('/api/etymology', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ word }),
+	});
+	resultSection.style.display = 'block';
+	definition.innerText = "";
+	const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+	while (true) {
+		const { value, done } = await reader.read();
+		if (done) {
+			console.log('Stream done');
+			break;
+		}
+		definition.innerText += value;
+	}
+});
---
+++ workers/typescript-based-api/src/index.ts
@@ -0,0 +1,37 @@
+import { Hono } from "hono";
+import { streamText } from "hono/streaming";
+import { events } from 'fetch-event-stream';
+
+
+type Bindings = {
+	[key in keyof CloudflareBindings]: CloudflareBindings[key];
+};
+
+
+const app = new Hono<{Bindings: Bindings}>();
+
+app.post("/api/etymology", async(c) => {
+	const payload = await c.req.json();
+	const eventStream = await c.env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
+		messages:[
+			{role: "system", content: `You are an etymologist.
+			The user is going to give you a word, and you should explain it's history and roots.
+			If you aren't sure let them know and encourage them to check the spelling.
+			Express your love for language. Keep things succint.
+			`},
+			{role: "user", content: payload.word}
+		],
+		stream: true
+	});
+	return streamText(c, async (stream) => {
+		const chunks = events(new Response(eventStream));
+		for await (const chunk of chunks) {
+			if (chunk.data !== undefined && chunk.data !== '[DONE]') {
+				const data = JSON.parse(chunk.data);
+				stream.write(data.response);
+			}
+		}
+	})
+});
+
+export default app;
