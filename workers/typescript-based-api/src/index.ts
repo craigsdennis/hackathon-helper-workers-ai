4e068c8de8050e87f09ed8a158ea79621e4c4797
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-09-07 12:46:45 -0700

    adjusted index.ts file to be for useless recipes

--- workers/typescript-based-api/src/index.ts
+++ workers/typescript-based-api/src/index.ts
@@ -1,22 +1,21 @@
-import { Hono } from "hono";
-import { streamText } from "hono/streaming";
+import { Hono } from 'hono';
+import { streamText } from 'hono/streaming';
 import { events } from 'fetch-event-stream';
 
+const app = new Hono<{ Bindings: Env }>();
 
-const app = new Hono<{Bindings: Env}>();
-
-app.post("/api/etymology", async(c) => {
+app.post('/api/etymology', async (c) => {
 	const payload = await c.req.json();
-	const eventStream = await c.env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
-		messages:[
-			{role: "system", content: `You are an etymologist.
-			The user is going to give you a word, and you should explain it's history and roots.
-			If you aren't sure let them know and encourage them to check the spelling.
-			Express your love for language. Keep things succint.
-			`},
-			{role: "user", content: payload.word}
+	const eventStream = await c.env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
+		messages: [
+			{
+				role: 'system',
+				content: `You give bad recipes. The user is going to input ingredients, and you are going to provide the worst recipe ever. Include the calories and nutrients of the dish. Unsuccessfully attempt to hide that this is a bad recipe.
+			`,
+			},
+			{ role: 'user', content: payload.word },
 		],
-		stream: true
+		stream: true,
 	});
 	return streamText(c, async (stream) => {
 		const chunks = events(new Response(eventStream as ReadableStream));
@@ -26,7 +25,7 @@ app.post("/api/etymology", async(c) => {
 				stream.write(data.response);
 			}
 		}
-	})
+	});
 });
 
 export default app;
